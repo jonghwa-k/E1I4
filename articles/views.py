18f2fc6e974580310dee3aa889d1aa10e6957a30
SH_kim <dal2018@naver.com>
2024-09-12 11:08:26 +0900

    feat: 글목록조회 구현
    -view.py 의 AriticleCreateAPIView에서 로그인상태가 아니어도
    글목록조회는 가능하도록 설정

--- articles/views.py
+++ articles/views.py
@@ -4,10 +4,23 @@ from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework import status
 from .serializers import ArticleSerializer
+from .models import Article
+from rest_framework.permissions import AllowAny
+
 
 class AriticleCreateAPIView(APIView):
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [permission() for permission in self.permission_classes]
+    
     def post(self, request):
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(author=request.user)  
             return Response(serializer.data, status=201) 
+        
+    def get(self, request):
+        articles = Article.objects.all().order_by('-id')
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
