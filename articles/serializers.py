ad1b215c0361ab44fe5204a54be239acbf87cea3
SH_kim <dal2018@naver.com>
2024-09-12 10:32:25 +0900

    feat: 아티클 생성 시작
    -url.py 에 생성 url 추가, static 추가
    -view.py AriticleCreateAPIView 작성(시작)
    -model.py Ariticle 작성

---
+++ articles/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+# class ProductSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Ariticle
+#         fields = "__all__"
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework import status
 
-# Create your views here.
+class AriticleCreateAPIView(APIView):
+    def post(self, request):
+        pass
