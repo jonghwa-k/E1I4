683b0125dec41a734e42e3d16e3075feefe96c50
jonghwa <on9236@naver.com>
2024-09-13 12:19:17 +0900

    feat: 내 프로필 조회 추가 - 내가 좋아요 한 글 확인 - 내가 좋아요 한 댓글 확인 - accounts 시리얼라이저에 추가

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -11,6 +11,8 @@ class UserSerializer(serializers.ModelSerializer):
 class UserProfileSerializer(UserSerializer):
     articles = ArticleSerializer(many=True, read_only=True)
     profile_comments = CommentSerializer(many=True, read_only=True)
-    Like_articles = ArticleSerializer(many=True, read_only=True)
+    like_articles = ArticleSerializer(many=True, read_only=True, source='like_article')
+    like_comments = CommentSerializer(many=True, read_only=True, source='like_comment')
     class Meta(UserSerializer.Meta):
-        fields = UserSerializer.Meta.fields + ('created_at', 'bio', 'articles', 'profile_comments', 'Like_articles',)
+        fields = (UserSerializer.Meta.fields +
+                  ('created_at', 'bio', 'articles', 'profile_comments', 'like_articles','like_comments',))
