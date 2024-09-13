034f70461eda65734a1bb4aac71c86373e673f78
jonghwa <on9236@naver.com>
2024-09-13 10:33:47 +0900

    feat: 프로필 수정 - 프로필 수정 로직 구현

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -11,5 +11,6 @@ class UserSerializer(serializers.ModelSerializer):
 class UserProfileSerializer(UserSerializer):
     articles = ArticleSerializer(many=True, read_only=True)
     profile_comments = CommentSerializer(many=True, read_only=True)
+    Like_articles = ArticleSerializer(many=True, read_only=True)
     class Meta(UserSerializer.Meta):
-        fields = UserSerializer.Meta.fields + ('created_at', 'bio', 'articles', 'profile_comments')
+        fields = UserSerializer.Meta.fields + ('created_at', 'bio', 'articles', 'profile_comments', 'Like_articles',)
