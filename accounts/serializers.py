e9d3e7073a8d16a5a4a01ed1fd11913663e18cea
temfarming <temfarming@naver.com>
2024-09-13 18:51:26 +0900

    사용자가 작성한 게시글 수 + 게시글의 좋아요 수를 수치화하여 사용자에게 포인트 부여 기능 완료

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import User
 from articles.serializers import ArticleSerializer, CommentSerializer
+from articles.models import Article
+from django.db.models import Sum
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,6 +16,11 @@ class UserProfileSerializer(UserSerializer):
     profile_comments = CommentSerializer(many=True, read_only=True)
     like_article_list = serializers.SerializerMethodField()
     like_comment_list = serializers.SerializerMethodField()
+    karma = serializers.SerializerMethodField()  
+
+    def get_articles(self, obj):
+        articles = obj.article_set.all()  
+        return ArticleSerializer(articles, many=True).data
 
     def get_like_article_list(self, obj):
         like_article = obj.like_article.all()
@@ -23,5 +30,10 @@ class UserProfileSerializer(UserSerializer):
         like_comment = obj.like_comment.all()
         return CommentSerializer(like_comment, many=True).data
 
+    def get_karma(self, obj):
+        article_count = Article.objects.filter(author=obj).count()
+        total_likes = Article.objects.filter(author=obj).aggregate(total_likes=Sum('likes'))['total_likes'] or 0
+        return article_count + total_likes
+
     class Meta(UserSerializer.Meta):
-        fields = UserSerializer.Meta.fields + ('created_at', 'bio', 'articles', 'profile_comments', 'like_article_list', 'like_comment_list')
+        fields = UserSerializer.Meta.fields + ('created_at', 'bio', 'karma', 'articles', 'profile_comments', 'like_article_list', 'like_comment_list' )
--- articles/serializers.py
+++ articles/serializers.py
@@ -19,7 +19,7 @@ class CommentSerializer(serializers.ModelSerializer):
         read_only_fields = ("article",)
 
 class ArticleSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')  # 작성자 필드를 읽기 전용으로 설정
+    author = serializers.ReadOnlyField(source='author.username')  
     like_count = serializers.SerializerMethodField()
     comments = CommentSerializer(many=True, read_only=True)
     category = serializers.ChoiceField(choices=[('News', 'News'), ('자유게시판', '자유게시판')])
@@ -29,4 +29,4 @@ class ArticleSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Article
-        fields = ('id',  "category", "title", "image","content", "author",  "url", "like_count", "comments")
+        fields = ('id',  "category", "title", "image","content", "author", "url", "like_count", "comments")
